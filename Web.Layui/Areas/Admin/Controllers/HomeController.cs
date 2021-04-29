5672dc7550892c342aa03107640367118a8b55b1
LuoCore <luoCore@hotmail.com>
2021-04-29 18:50:20 +0800

    加入layui框架

---
+++ Web.Layui/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Layui.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+    }
+}
